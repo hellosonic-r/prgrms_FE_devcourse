8888b20e917747dbb8ce88138beed65c42db6147
hellosonic-r <hellommmh@gmail.com>
2023-10-07 20:51:13 +0900

    feat : 버튼 클릭 횟수 Coun하기

--- 컴포넌트 방식으로 생각하기/혼자 해보기/toggle.js
+++ 컴포넌트 방식으로 생각하기/혼자 해보기/toggle.js
@@ -1,13 +1,33 @@
 
 const $target = document.querySelector('#app');
 
-function ToggleButton( { $target , text}) {
+function ToggleButton( { $target , text, clickCount }) {
 
     const $button = document.createElement('button');
-    $target.appendChild($button);
+    let isInit = false;
 
     this.render = () => {
-        $button.textContent = text;
+        $button.textContent = `${text} : ${clickCount}번 클릭`;
+
+        if (!isInit) {
+            $target.appendChild($button);
+
+            $button.addEventListener('click', ()=> {
+                if ($button.style.color === 'black') {
+                    $button.style.color = 'red';
+        
+                } else {
+                    $button.style.color = 'black';
+                }
+                clickCount++
+                $button.textContent = `${text} : ${clickCount}번 클릭`;
+                if (clickCount%3 === 0) {
+                    alert('세 번째 클릭');
+                } 
+
+            })
+            isInit = true;
+        }
     }
 
     this.render();
@@ -16,10 +36,12 @@ function ToggleButton( { $target , text}) {
 
 new ToggleButton( {
     $target : $target,
-    text : 'button1'
+    text : 'button1',
+    clickCount : 0,
 } );
 
 new ToggleButton( {
     $target : $target,
-    text : 'button2'
+    text : 'button2',
+    clickCount : 0,
 })
