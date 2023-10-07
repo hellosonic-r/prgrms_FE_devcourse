9efcedcd74b95adb1019d13a804c150a6c600327
hellosonic-r <hellommmh@gmail.com>
2023-10-07 20:11:16 +0900

    feat : ToggleButton로 버튼 만들기(컴포넌트 단위)

--- 컴포넌트 방식으로 생각하기/혼자 해보기/toggle.js
+++ 컴포넌트 방식으로 생각하기/혼자 해보기/toggle.js
@@ -1,33 +1,25 @@
-// 버튼을 3개를 만든다.
-
-// 버튼 1
-
-const $container = document.createDocumentFragment();
-
-
-const $button1 = document.createElement('button');
-$button1.textContent = 'button1';
 
 const $target = document.querySelector('#app');
 
-// 버튼 2
-const $button2 = document.createElement('button');
-$button2.textContent = 'button2';
+function ToggleButton( { $target , text}) {
 
-// 버튼 3
-const $button3 = document.createElement('button');
-$button3.textContent = 'button3';
+    const $button = document.createElement('button');
+    $target.appendChild($button);
 
-$container.appendChild($button1);
-$container.appendChild($button2);
-$container.appendChild($button3);
+    this.render = () => {
+        $button.textContent = text;
+    }
 
-$target.appendChild($container);
+    this.render();
+}
 
-document.querySelectorAll('button').forEach($button => {
-    addEventListener('click', (e) => {
-        const { target } = e;
-        target.style.textDecoration === 'line-through' ? target.style.textDecoration = 'none' : target.style.textDecoration = 'line-through';
-    })
+
+new ToggleButton( {
+    $target : $target,
+    text : 'button1'
+} );
+
+new ToggleButton( {
+    $target : $target,
+    text : 'button2'
 })
-
