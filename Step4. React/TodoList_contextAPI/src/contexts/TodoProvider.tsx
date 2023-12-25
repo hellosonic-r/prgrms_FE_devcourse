2ba555eaf9e854a04404423f0f73401977dec590
hellosonic-r <hellommmh@gmail.com>
2023-12-25 23:05:19 +0900

    feat : contextAPI Provider

---
+++ Step4. React/TodoList_contextAPI/src/contexts/TodoProvider.tsx
@@ -0,0 +1,49 @@
+import { createContext, useState } from "react";
+import { v4 } from "uuid";
+
+interface ITodoProps {
+  id: string;
+  content: string;
+  isComplete: boolean;
+}
+
+interface IChildrenProps {
+  children: React.ReactNode;
+}
+
+const TodoContext = createContext<{
+  todos: ITodoProps[];
+  addTodo: (content: string) => void;
+  deleteTodo: (id: string) => void;
+}>({
+  todos: [],
+  addTodo: () => {},
+  deleteTodo: () => {},
+});
+
+const TodoProvider = ({ children }: IChildrenProps) => {
+  const [todos, setTodos] = useState<ITodoProps[]>([]);
+
+  const addTodo = (content: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: v4(),
+        content,
+        isComplete: false,
+      },
+    ]);
+  };
+
+  const deleteTodo = (id: string) => {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
+  };
+  return (
+    <TodoContext.Provider value={{ todos, addTodo, deleteTodo }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoProvider;
