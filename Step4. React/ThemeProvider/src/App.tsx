a0ed7912f14da5bfb64e5b6a3e3618466ca45fc2
hellosonic-r <hellommmh@gmail.com>
2023-12-27 23:09:42 +0900

    feat : ThemeProvider theme.tsx 사용 예시

--- Step4. React/ThemeProvider/src/App.tsx
+++ Step4. React/ThemeProvider/src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Button from './components/Button/Button';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from './styles/Theme';
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.color.blue};
+`;
+
+const Content = styled.span`
+  font-size: ${({ theme }) => theme.font.l};
+`;
 
 const App = () => {
   return (
     <div>
       <Button></Button>
+      <ThemeProvider theme={theme}>
+        <Title>제목입니다.</Title>
+        <Content>내용입니다.</Content>
+      </ThemeProvider>
     </div>
   );
 };
---
+++ Step4. React/ThemeProvider/src/styles/Theme.ts
@@ -0,0 +1,18 @@
+const color: object = {
+  black: '#000000',
+  grey: '#999999',
+  blue: '#000080',
+};
+
+const font: object = {
+  s: '1rem',
+  m: '2rem',
+  l: '4rem',
+};
+
+const theme = {
+  color,
+  font,
+};
+
+export default theme;
