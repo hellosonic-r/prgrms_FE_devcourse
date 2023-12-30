4cc163a8b0fbf8177a9b90e3503df2f125df39f6
hellosonic-r <hellommmh@gmail.com>
2023-12-30 23:37:50 +0900

    feat : bottomnavbar 연습

--- Step4. React/NavBar 개발 연습/src/BottomNavBar/BottomNavBar.tsx
+++ Step4. React/NavBar 개발 연습/src/BottomNavBar/BottomNavBar.tsx
@@ -1,26 +1,31 @@
-import styled from 'styled-components';
-import { Icon } from '..';
+import styled, { useTheme } from 'styled-components';
+import Icon from '../Icon/Icon';
 import {
   CHANNEL_ICON,
   HOME_ICON,
   SEARCH_ICON,
   USER_ICON,
 } from '@/constants/icons';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useCallback, useMemo } from 'react';
+import { APP_MAX_WIDTH } from '@/constants/uiConstants';
 
-const BottomNavBarWrapper = styled.div`
-  /* height: 3rem; */
-  background-color: white;
+const BottomNavBarWrapper = styled.nav`
+  position: fixed;
+  bottom: 0;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  max-width: ${APP_MAX_WIDTH}px;
+  width: 100%;
+  height: 3rem;
+  background-color: white;
 `;
 const IconWrapper = styled.div`
-  /* border: 1px solid black; */
   flex-grow: 1;
   justify-content: center;
   align-items: center;
-  transition: background-color 0.3s ease;
+  transition: background-color 0.5s ease;
   &:hover {
     background-color: #eaeaea;
     cursor: pointer;
@@ -28,26 +33,65 @@ const IconWrapper = styled.div`
 `;
 
 const BottomNavBar = () => {
-  // 채팅 중인 페이지는
-  const params = useParams(); // 아이디가 들어가있으면 채팅창임
-  const showNavBar = params;
-  console.log(params);
-  console.log(window.location.pathname);
+  const { pathname } = useLocation();
+  const NAV_INABLE_PATH = [
+    '/home',
+    '/channels',
+    '/profile',
+    '/search',
+    '/chats',
+    '/postdetail',
+    '/test',
+  ];
+
+  const currentPath = useMemo(() => '/' + pathname.split('/')[1], [pathname]);
+  const showNavBar = useMemo(
+    () => NAV_INABLE_PATH.includes(currentPath),
+    [currentPath]
+  );
+
+  const theme = useTheme();
+  const navigate = useNavigate();
+
+  const handleClick = useCallback(
+    (path: string) => {
+      if (path !== pathname) {
+        navigate(path);
+      }
+    },
+    [pathname]
+  );
 
   return (
     showNavBar && (
       <BottomNavBarWrapper>
-        <IconWrapper>
-          <Icon name={HOME_ICON} weight={250} />
+        <IconWrapper onClick={() => handleClick('/home')}>
+          <Icon
+            name={HOME_ICON}
+            color={currentPath === '/home' ? theme.symbol_color : ''}
+            weight={currentPath === '/home' ? 300 : 250}
+          />
         </IconWrapper>
-        <IconWrapper>
-          <Icon name={SEARCH_ICON} weight={250} />
+        <IconWrapper onClick={() => handleClick('/search')}>
+          <Icon
+            name={SEARCH_ICON}
+            color={currentPath === '/search' ? theme.symbol_color : ''}
+            weight={currentPath === '/search' ? 300 : 250}
+          />
         </IconWrapper>
-        <IconWrapper>
-          <Icon name={CHANNEL_ICON} weight={250} />
+        <IconWrapper onClick={() => handleClick('/channels')}>
+          <Icon
+            name={CHANNEL_ICON}
+            color={currentPath === '/channels' ? theme.symbol_color : ''}
+            weight={currentPath === '/channels' ? 300 : 250}
+          />
         </IconWrapper>
-        <IconWrapper>
-          <Icon name={USER_ICON} weight={250} />
+        <IconWrapper onClick={() => handleClick('/profile')}>
+          <Icon
+            name={USER_ICON}
+            color={currentPath === '/profile' ? theme.symbol_color : ''}
+            weight={currentPath === '/profile' ? 300 : 250}
+          />
         </IconWrapper>
       </BottomNavBarWrapper>
     )
