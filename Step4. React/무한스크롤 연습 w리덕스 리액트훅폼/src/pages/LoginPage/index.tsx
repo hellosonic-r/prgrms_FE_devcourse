83cdd54969cd2fe83607cd10918537357c0d3268
hellosonic-r <hellommmh@gmail.com>
2024-01-03 13:28:55 +0900

    feat : react-hook-form 으로 로그인 페이지 구현

--- Step4. React/무한스크롤 연습 w리덕스 리액트훅폼/src/pages/LoginPage/index.tsx
+++ Step4. React/무한스크롤 연습 w리덕스 리액트훅폼/src/pages/LoginPage/index.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import { useForm, FieldValues, FieldErrors } from 'react-hook-form';
 import styled from 'styled-components';
 
 const LoginPageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
   height: 100vh;
 `;
+
+const StyledForm = styled.form`
+  display: flex;
+  width: 70%;
+  flex-direction: column;
+`;
 const LoginPage = () => {
-  return <LoginPageWrapper>LoginPage</LoginPageWrapper>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: 'onChange' });
+
+  const onValid = (data: FieldValues) => {
+    console.log('성공', data);
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log('실패', errors);
+  };
+  return (
+    <LoginPageWrapper>
+      <StyledForm onSubmit={handleSubmit(onValid, onInvalid)}>
+        <input
+          type="email"
+          placeholder="email@email.com"
+          {...register('email', { required: '이메일을 입력해주세요' })}
+        />
+        <span>
+          {errors.email?.message ? errors.email.message.toString() : ' .'}
+        </span>
+        <input
+          type="password"
+          placeholder="********"
+          {...register('password', {
+            required: '비밀번호를 입력해주세요',
+            minLength: {
+              value: 8,
+              message: '비밀번호는 8자 이상이 되어야합니다.',
+            },
+          })}
+        />
+        <span>
+          {errors.password?.message
+            ? errors.password.message.toString()
+            : ' . '}
+        </span>
+        <button>Login</button>
+      </StyledForm>
+    </LoginPageWrapper>
+  );
 };
 
 export default LoginPage;
