4e86cc32fa6130af73df01ee6d1d08bb920985b7
hellosonic-r <hellommmh@gmail.com>
2024-01-05 13:28:34 +0900

    feat : 합친 리듀서들을 단일 store로 제공

---
+++ Step4. React/Modal 연습 w리덕스/src/store/index.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import rootReducer from './reducers';
+
+const createStore = () => {
+  const store = configureStore({
+    reducer: rootReducer,
+  });
+
+  return store;
+};
+
+const store = createStore();
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
